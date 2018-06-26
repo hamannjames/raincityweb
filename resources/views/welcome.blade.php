add08f7e22fed95258c038c009f75c7d5929a9de
James Hamman <jameshamman@Daniels-MacBook-Pro-2.local>
2018-06-25 17:50:18 -0700

    try changing home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <main>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -77,19 +77,15 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <section id="home_wrapper">
+                <div id="home_container">
+                    <div id="home_overlay--video">
+                        <video>
+                        </video>
+                    </div>
                 </div>
+            </section>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        </main>
     </body>
 </html>
