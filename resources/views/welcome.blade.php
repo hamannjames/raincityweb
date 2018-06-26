a129428137b27f837e9ac294843d24e88a963a52
James Hamman <jameshamman@Daniels-MacBook-Pro-2.local>
2018-06-25 19:19:15 -0700

    new index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
+                font-size: 100%;
             }
 
             .full-height {
@@ -62,6 +63,56 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            #home_container {
+                position: relative;
+                width: 100%;
+                height: 100%;
+                min-height: 100vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            #home_overlay--video {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+            }
+
+            #home_overlay--video video {
+                position: absolute;
+                min-width: 100%;
+                min-height: 100%;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+
+            #home_overlay--filter {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0,0,0,.4);
+            }
+
+            #home_overlay--video.wide video {
+                width: 100%;
+            }
+
+            #home_overlay--video.box video {
+                height: 100%;
+            }
+
+            #main-content_heading {
+                margin: 0;
+                position: relative;
+                color: #fff;
+                font-size: 2rem;
+            }
         </style>
     </head>
     <body>
@@ -80,12 +131,59 @@
             <section id="home_wrapper">
                 <div id="home_container">
                     <div id="home_overlay--video">
-                        <video>
+                        <video autoplay="true" loop="true">
+                            <source src="{{ url('/storage/media/video/rainy_cars.webm') }}" type="video/webm">
+                            <source src="{{ url('/storage/media/video/rainy_cars.mp4') }}" type="video/mp4">
                         </video>
                     </div>
+                    <div id="home_overlay--filter"></div>
+
+                    <h1 id="main-content_heading">Rain City Web</h1>
                 </div>
             </section>
 
         </main>
+
+        <script>
+            function debounce(func, wait, immediate) {
+                var timeout;
+                return function() {
+                    var context = this, args = arguments;
+                    var later = function() {
+                        timeout = null;
+                        if (!immediate) func.apply(context, args);
+                    };
+                    var callNow = immediate && !timeout;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(later, wait);
+                    if (callNow) func.apply(context, args);
+                };
+            };
+
+            let overlayVideo = document.getElementById('home_overlay--video');
+
+            window.aspectRatio = {
+                aspect: '',
+
+                determineAspect() {
+                    let aspect = (window.innerWidth / window.innerHeight > 16/9) ? 'wide' : 'box';
+
+                    if (aspect !== window.aspectRatio.aspect) {
+                        window.aspectRatio.aspect = aspect;
+                        window.dispatchEvent(new CustomEvent('aspectchange', { detail: aspect }))
+                    }
+                }
+            }
+
+            window.addEventListener('resize', debounce(window.aspectRatio.determineAspect, 100));
+            window.addEventListener('aspectchange', function(e){
+                let add = e.detail;
+                let remove = (e.detail === 'wide') ? 'box' : 'wide';
+                overlayVideo.classList.remove(remove);
+                overlayVideo.classList.add(add);
+            });
+
+            window.aspectRatio.determineAspect();
+        </script>
     </body>
 </html>
