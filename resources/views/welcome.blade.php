69fcc3ac2e66e0cb50c34e7648087488acf52270
James Hamann <hamann.james87@gmail.com>
2018-07-31 23:21:55 -0700

    seo tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Showcasing the web from the rainy city">
 
-        <title>Laravel</title>
+        <title>Rain City Web</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600" rel="stylesheet">
